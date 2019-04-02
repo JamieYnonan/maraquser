c9744fb926026dc02e04a0651ac9ae15f5d4bb12
Jamie Ynonan <jamiea31@gmail.com>
2019-04-02 00:08:36 -0500

    test(application): create tests show user handler

--- app/src/Mu/Application/User/ShowUserQuery.php
+++ app/src/Mu/Application/User/ShowUserQuery.php
@@ -6,7 +6,7 @@ final class ShowUserQuery
 {
     private $userId;
 
-    public function __construct($userId)
+    public function __construct(string $userId)
     {
         $this->userId = $userId;
     }
---
+++ app/tests/Mu/Application/User/ShowUserHandlerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Mu\Application\User;
+
+use Mu\Domain\Model\Role\Role;
+use Mu\Domain\Model\Role\RoleId;
+use Mu\Domain\Model\User\Email;
+use Mu\Domain\Model\User\LastName;
+use Mu\Domain\Model\User\Name;
+use Mu\Domain\Model\User\Password;
+use Mu\Domain\Model\User\User;
+use Mu\Domain\Model\User\UserException;
+use Mu\Domain\Model\User\UserId;
+use Mu\Domain\Model\User\UserService;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class ShowUserHandlerTest extends TestCase
+{
+    /**
+     * @var MockObject
+     */
+    private $userServiceMock;
+
+    private $query;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->userServiceMock = $this
+            ->getMockBuilder(UserService::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['byIdOrFail'])
+            ->getMock();
+
+        $this->query = new ShowUserQuery((new UserId())->value());
+    }
+
+    public function testHandler()
+    {
+        $this->userServiceMock->method('byIdOrFail')
+            ->willReturn($this->createUser());
+
+        $handler = $this->createHandler();
+        $this->assertInstanceOf(
+            User::class,
+            $handler->handler($this->query)
+        );
+    }
+
+    private function createUser(): User
+    {
+        return new User(
+            new UserId(),
+            new Name('name'),
+            new LastName('last name'),
+            new Email('email@mail.com'),
+            Password::byCleanPassword('123456'),
+            new Role(new RoleId(), new \Mu\Domain\Model\Role\Name('role'))
+        );
+    }
+
+    private function createHandler(): ShowUserHandler
+    {
+        return new ShowUserHandler($this->userServiceMock);
+    }
+
+    /**
+     * @expectedException \Mu\Domain\Model\User\UserException
+     */
+    public function testThrowExceptionInvalidUser()
+    {
+        $this->userServiceMock->method('byIdOrFail')
+            ->willThrowException(UserException::notExistsById());
+
+        $handler = $this->createHandler();
+        $handler->handler($this->query);
+    }
+}
